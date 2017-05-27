94d54d0a23e48fb989a53e38098364aeaad69a38
adhir <adhir.aima@gmail.com>
2017-05-28 01:43:54 +0530

    removed localhost from server config object

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ var routes = require('./config/routes')
 const server = new Hapi.Server();
 
 server.connection({
-    host: 'localhost',
     port: process.env.PORT || 5000
 })
 
 server.route(routes)
 
-server.start(() => console.log('started at: ${server.info.uri}'))
+server.start(() => console.log('started at: ' + server.info.uri))
