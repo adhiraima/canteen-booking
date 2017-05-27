4ff8f88c917bb6f5cc66934c5b36c59a9f548cac
adhir <adhir.aima@gmail.com>
2017-05-28 02:28:49 +0530

    favicon 2nd add

--- config/routes/index.js
+++ config/routes/index.js
@@ -15,5 +15,21 @@ module.exports = [
                                                                                   console.log('slots handler');
                                                                                   reply('slots ' + request.params.date)
                                                                                }
+    },
+    {
+    path: '/favicon.ico',
+    method: 'GET',
+    config: {
+      auth: false,
+      cache: {
+        expiresIn: 1000*60*60*24*21
+      }
+    },
+    handler: function(request, reply) {
+      if (!options.path) {
+        return reply().code(204).type('image/x-icon');
+      }
+      reply(null, fs.createReadStream('./images/favicon')).code(200).type('image/x-icon');
     }
+  }
 ];
